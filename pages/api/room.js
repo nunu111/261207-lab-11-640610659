67801ef1978a43a9f74d995ae2a3edca0dd83c99
nunu111 <nunu111za@hotmail.com>
2022-09-29 15:50:38 +0700

    lab11 t1

--- pages/api/room.js
+++ pages/api/room.js
@@ -10,6 +10,14 @@ export default function roomRoute(req, res) {
     });
   }
   const chatrooms = readChatRoomsDB();
+  const rooms = [];
+  for (const key of chatrooms) {
+    rooms.push({
+      roomId: key.roomId,
+      roomName: key.roomName,
+    });
+  }
 
+  return res.json({ ok: true, rooms });
   //create room data and return response
 }
--- pages/api/user/login.js
+++ pages/api/user/login.js
@@ -7,14 +7,40 @@ export default function login(req, res) {
     const { username, password } = req.body;
 
     //validate body
-
+    if (
+      typeof username !== "string" ||
+      typeof password !== "string" ||
+      username.length === 0 ||
+      password.length === 0
+    )
+      return res
+        .status(400)
+        .json({ ok: false, message: "Username or password cannot be empty" });
     const users = readUsersDB();
-
+    const foundUser = users.find(
+      (x) => x.username === username && bcrypt.compareSync(password, x.password)
+    );
     //find users with username, password
-
+    if (!foundUser)
+      return res
+        .status(400)
+        .json({ ok: false, message: "Invalid username or password" });
     const secret = process.env.JWT_SECRET;
-
+    const token = jwt.sign(
+      {
+        username: foundUser.username,
+        isAdmin: foundUser.isAdmin,
+      },
+      secret,
+      { expiresIn: "1800s" }
+    );
     //sign token
+    return res.json({
+      ok: true,
+      username: foundUser.username,
+      isAdmin: foundUser.isAdmin,
+      token,
+    });
 
     //return response
   }
